2e637ba8e2b0b30cc64281d55b11998b56bbc89c
Radule <master.daca09@gmail.com>
2019-07-06 17:21:08 +0200

    Added generated music files

--- lstm_generate_music.py
+++ lstm_generate_music.py
@@ -1,4 +1,4 @@
-from music21 import note, chord, instrument, stream
+from music21 import note, chord, stream
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -7,24 +7,6 @@ from keras.layers import Activation
 import pickle
 import random
 import numpy as np
-
-
-def map_int_to_notes(pitches):
-    mapped_int = {}
-    counter = 0
-    for music_element in pitches:
-        mapped_int[counter] = music_element
-        counter += 1
-    return mapped_int
-
-
-def map_notes_to_int(pitches):
-    mapped_notes = {}
-    counter = 0
-    for music_element in pitches:
-        mapped_notes[music_element] = counter
-        counter += 1
-    return mapped_notes
 
 
 def create_model(input, total_pitches, weights):
@@ -48,33 +30,30 @@ def create_model(input, total_pitches, weights):
     return model
 
 
-def create_list_of_sequences(notes, mapped_notes, notes_sequence_length, total_pitches):
+def create_list_of_sequences(notes, notes_sequence_length, total_pitches):
     list_of_sequences = []
-    output = []
     for i in range(0, len(notes) - notes_sequence_length, 1):
         input_sequence = notes[i:i + notes_sequence_length]
-        output_of_sequence = notes[i + notes_sequence_length]
-        list_of_sequences.append([mapped_notes[elem] for elem in input_sequence])
-        output.append(mapped_notes[output_of_sequence])
+        list_of_sequences.append([pitches.index(elem) for elem in input_sequence])
 
     num_of_sequences = len(list_of_sequences)
 
-    norm_input = np.reshape(list_of_sequences, (num_of_sequences, notes_sequence_length, 1))
-    norm_input = norm_input / total_pitches
+    input = np.reshape(list_of_sequences, (num_of_sequences, notes_sequence_length, 1))
+    input = input / total_pitches
 
-    return list_of_sequences, norm_input
+    return list_of_sequences, input
 
 
-def generate_sequence(model, mapped_int, input, total_pitches, num_of_notes):
+def generate_sequence(model, input, pitches, num_of_notes):
     random_sequence = random.randint(0, len(input) - 1)
     sequence = input[random_sequence]
     generated_sequence = []
     for note in range(num_of_notes):
         input_sequence = np.reshape(sequence, (1, len(sequence), 1))
-        input_sequence = input_sequence / total_pitches
+        input_sequence /= len(pitches)
         output = model.predict(input_sequence, verbose=0)
         note_as_int = np.argmax(output)
-        output_note = mapped_int[note_as_int]
+        output_note = pitches[note_as_int]
         generated_sequence.append(output_note)
         sequence.append(note_as_int)
         sequence = sequence[1:len(sequence)]
@@ -106,13 +85,10 @@ def generate_midi(generated_sequence):
 
 if __name__ == '__main__':
     notes_sequence_length = 100
-    input = pickle.load(open("input_binary.p", "rb"))
-    notes = pickle.load(open("notes.p", "rb"))
-    pitches = pickle.load(open("pitches.p", "rb"))
-    weights = "weights-18-6.1185.hdf5"
-    mapped_int = map_int_to_notes(pitches)
-    mapped_notes = map_notes_to_int(pitches)
-    list_of_sequences, norm_inp = create_list_of_sequences(notes, mapped_notes, notes_sequence_length, len(pitches))
-    model = create_model(norm_inp, len(pitches), weights)
-    generated_sequence = generate_sequence(model, mapped_int, list_of_sequences, len(pitches), 500)
+    notes = pickle.load(open("notes/notes.p", "rb"))
+    pitches = pickle.load(open("notes/pitches.p", "rb"))
+    list_of_sequences, input = create_list_of_sequences(notes, notes_sequence_length, len(pitches))
+    weights = "weights/weights-60-0.0378.hdf5"
+    model = create_model(input, len(pitches), weights)
+    generated_sequence = generate_sequence(model, list_of_sequences, pitches, 500)
     generate_midi(generated_sequence)
--- lstm_train.py
+++ lstm_train.py
@@ -1,70 +1,10 @@
-from music21 import note, chord
-from joblib import Parallel, delayed
-import numpy as np
-import os
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.layers import Activation
 from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
 import pickle
-from AIMusicGenerator.pixel_cnn_related.midi_to_img import open_midi, load_from_bin, get_input_paths
-
-
-def extract_line(song, instruments_list):
-    part_stream = song.parts.stream()
-
-    for instrument in instruments_list:
-        for part in part_stream:
-            if instrument == part.partName:
-                return part
-
-    # some midi files have no instrument names, so sample the first part
-    return part_stream[0]
-
-
-def process_midi_file(path, instruments_list):
-    notes = []
-    try:
-        song = open_midi(path)
-        print(path)
-        instrument_line = extract_line(song, instruments_list)
-        for music_element in instrument_line:
-            if isinstance(music_element, note.Note):
-                notes.append(str(music_element.pitch))
-            elif isinstance(music_element, chord.Chord):
-                notes.append('.'.join(str(n) for n in music_element.pitches))
-        return notes
-    except:
-        return []
-
-
-def get_notes(paths, instruments_list):
-    results = Parallel(n_jobs=-1)(delayed(process_midi_file)(p, instruments_list) for p in paths)
-    return results
-
-
-def create_input_and_output(notes_sequence_length, pitches, notes):
-    input, output = [], []
-
-    for i in range(0, len(notes) - notes_sequence_length, 1):
-        input_sequence = notes[i:i + notes_sequence_length]
-        output_of_sequence = notes[i + notes_sequence_length]
-        input.append([pitches.index(char) for char in input_sequence])
-        output.append(pitches.index(output_of_sequence))
-
-    num_of_sequences = len(input)
-    input = np.reshape(input, (num_of_sequences, notes_sequence_length, 1))
-    input = input / len(pitches)
-    output = np_utils.to_categorical(output)
-
-    print(input)
-    print(output)
-
-    return input, output
 
 
 def create_model(input, total_pitches):
@@ -96,17 +36,8 @@ def train_network(model, input, output):
 
 
 if __name__ == '__main__':
-    notes_sequence_length = 100
-    instruments = load_from_bin(os.path.join("instruments", "instruments_lstm.bin"))
-
-    notes = pickle.load(open(os.path.join("notes", "notes.p"), "rb"))
-    pitches = sorted(set(notes))
-
-    input, output = create_input_and_output(notes_sequence_length, pitches, notes)
-    print(input)
-    print(output)
-    # Radule TODO: sacuvaj input i output sa pickle i stavi da se trenira
-    """
+    pitches = pickle.load(open("notes/pitches.p", "rb"))
+    input = pickle.load(open("notes/input_binary.p", "rb"))
+    output = pickle.load(open("notes/output_binary.p", "rb"))
     model = create_model(input, len(pitches))
     train_network(model, input, output)
-    """
